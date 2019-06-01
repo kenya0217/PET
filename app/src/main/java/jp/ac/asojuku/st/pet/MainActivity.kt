0c2114600bbdabf84a1302aaa64f4926953be57a
ASO1701009 <1701009@st.asojuku.ac.jp>
2019-06-01 18:30:57 +0900

    a

--- app/src/main/java/jp/ac/asojuku/st/pet/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/st/pet/MainActivity.kt
@@ -2,11 +2,35 @@ package jp.ac.asojuku.st.pet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.ListView
+import android.widget.TextView
+import kotlin.arrayOf as arrayOf
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var pet = Pet(
+            intent.getStringExtra(“png”),
+        intent.getIntExtra(“dec”,222),
+        intent.getIntExtra(“border”,222),
+        intent.getStringExtra(“name”))
+        val button = findViewById(R.id.button) as Button
+
+        button.setOnClickListener {
+
+        }
+        val items = arrayOf("tintin","onon","manman","aa","bb","cc")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
+        ListView.adapter = adapter
+        ListView.setOnItemClickListener { adapterView, view, position, id ->
+
+        }
+        ListView.setVisibility(View.GONE);
     }
 }
